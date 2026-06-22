ede72f7d173072a1466fa43809c4b1aa23486464
CodexSandboxOffline <jeshminbimali@gmail.com>
2026-06-22 11:16:30 +0545

    Add more recipe examples

--- recipes.py
+++ recipes.py
@@ -9,5 +9,24 @@ RECIPES = [
         "name": "Burger",
         "ingredients": "Bun, Patty, Cheese",
     },
+    {
+        "id": 3,
+        "name": "Veggie Pasta",
+        "ingredients": "Pasta, Bell Pepper, Tomato, Garlic, Olive Oil",
+    },
+    {
+        "id": 4,
+        "name": "Chicken Curry",
+        "ingredients": "Chicken, Onion, Tomato, Ginger, Garlic, Spices",
+    },
+    {
+        "id": 5,
+        "name": "Fruit Salad",
+        "ingredients": "Apple, Banana, Grapes, Orange, Honey",
+    },
+    {
+        "id": 6,
+        "name": "Pancakes",
+        "ingredients": "Flour, Milk, Egg, Sugar, Baking Powder",
+    },
 ]
-
