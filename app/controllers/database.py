3e7f9c4d8ce33e8d4f8caa4ebcb4bf0a97319394
Jeshmin <jeshminbimali@gmail.com>
2026-07-02 22:02:51 +0545

    Clean up unused imports and variables

--- app/controllers/database.py
+++ app/controllers/database.py
@@ -1,11 +1,6 @@
-from app.database import create_tables, get_connection, seed_recipes
+from app.database import create_tables, seed_recipes
 
 
 def initialize_database():
     create_tables()
     seed_recipes()
-
-
-def recipe_count():
-    with get_connection() as db:
-        return db.execute("SELECT COUNT(*) FROM recipes").fetchone()[0]
